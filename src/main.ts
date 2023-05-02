197fa5e47ae35bdce109eee6dd0a96fa4e4abd86
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-01 19:42:29 -0600

    homework objetos e interfaces resuelto

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import './style.css';
 // Modulos
 // import '../src/topics/01-basic-types';
 // import '../src/topics/02-object-interfaces';
-import '../src/topics/03-functions';
+// import '../src/topics/03-functions';
+import '../src/topics/04-homework-types';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Hola Mundo</h1>
