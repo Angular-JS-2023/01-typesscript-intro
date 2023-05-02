83f1259089a38e79bfedf60afaaac8cfd0216c2a
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-01 19:03:20 -0600

    basic types

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import './style.css';
 
+// Modulos
+// import '../src/topics/01-basic-types';
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Hola Mundo</h1>
 `;
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,14 @@
+
+// Manejar const
+const name: string = 'Strider';
+// name = '123';
+
+// Puede ser un número o cadena de texto
+let hpPoints: number | string = 95;
+hpPoints = 'FULL';
+
+const isAlive: boolean = true;
+
+console.log({ name, hpPoints, isAlive });
+
+export {};
