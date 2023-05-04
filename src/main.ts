510a8bd5686b7b06bc55c59b38734beff907ec97
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-03 20:27:37 -0600

    desestructuracion de argumentos

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import './style.css';
 // import '../src/topics/02-object-interfaces';
 // import '../src/topics/03-functions';
 // import '../src/topics/04-homework-types';
-import '../src/topics/05-basic-destructuring';
+// import '../src/topics/05-basic-destructuring';
+import '../src/topics/06-function-destructuring';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Hola Mundo</h1>
